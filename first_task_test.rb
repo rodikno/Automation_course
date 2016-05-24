5b07d06d0a4c22ada8cbbe6b23d56e589bbac9cd
Rodion Baronov <rodikno@gmail.com>
2016-05-24 20:27:05 +0300

    Canged all @driver.find_element to help methods in TestClass

--- first_task_test.rb
+++ first_task_test.rb
@@ -17,16 +17,16 @@ class TestFirst < Test::Unit::TestCase
     register_user
 
     assert_equal(@driver.current_url, 'http://demo.redmine.org/my/account')
-    assert(@driver.find_element(:id, 'flash_notice'))
+    assert(find_element_by_id('flash_notice'))
   end
 
   def test_log_out
 
     register_user
 
-    @driver.find_element(:class, 'logout').click
+    find_element_by_class('logout').click
 
-    login_button = @driver.find_element(:class, 'login')
+    login_button = find_element_by_class('login')
 
     @wait.until{login_button.displayed?}
 
@@ -37,12 +37,12 @@ class TestFirst < Test::Unit::TestCase
   def test_log_in
     register_user
 
-    current_username = @driver.find_element(:class, 'user').text
+    current_username = find_element_by_class('user').text
 
     log_out
 
     log_in(current_username, 's0meP@ssw0rd')
-    assert_equal(current_username, @driver.find_element(:class, 'user').text)
+    assert_equal(current_username, find_element_by_class('user').text)
   end
 
   def test_change_password
@@ -52,17 +52,17 @@ class TestFirst < Test::Unit::TestCase
     new_password = 'pass_' + rand(9999).to_s
 
 
-    @driver.find_element(:class, 'icon-passwd').click
+    find_element_by_class('icon-passwd').click
 
     @wait.until {@driver.current_url == 'http://demo.redmine.org/my/password'}
 
-    @driver.find_element(:name, 'password').send_keys(old_password)
-    @driver.find_element(:name, 'new_password').send_keys(new_password)
-    @driver.find_element(:name, 'new_password_confirmation').send_keys(new_password)
-    @driver.find_element(:name, 'commit').click
+    find_element_by_name('password').send_keys(old_password)
+    find_element_by_name('new_password').send_keys(new_password)
+    find_element_by_name('new_password_confirmation').send_keys(new_password)
+    find_element_by_name('commit').click
 
     assert_equal(@driver.current_url, 'http://demo.redmine.org/my/account')
-    assert(@driver.find_element(:id, 'flash_notice'))
+    assert (find_element_by_id('flash_notice'))
   end
 
   def test_create_project
@@ -70,7 +70,7 @@ class TestFirst < Test::Unit::TestCase
 
     project_name = create_project
 
-    @wait.until{@driver.find_element(:id, 'flash_notice').displayed?}
+    @wait.until{find_element_by_id('flash_notice').displayed?}
 
     assert_equal("http://demo.redmine.org/projects/#{project_name}/settings", @driver.current_url)
   end
@@ -85,12 +85,12 @@ class TestFirst < Test::Unit::TestCase
 
     @driver.navigate.to("http://demo.redmine.org/projects/#{project_name}/versions/new?back_url=")
 
-    version_name_input = @driver.find_element(:id, 'version_name')
+    version_name_input = find_element_by_id('version_name')
 
     @wait.until{version_name_input.displayed?}
 
     version_name_input.send_keys(version_name)
-    @driver.find_element(:name, 'commit').click
+    find_element_by_name('commit').click
 
     assert_equal("http://demo.redmine.org/projects/#{project_name}/settings/versions", @driver.current_url)
   end
