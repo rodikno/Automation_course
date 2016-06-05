56e91dde549ef7b831cda32a96f1d5c1fa45acde
Rodion Baronov <rodikno@gmail.com>
2016-06-05 20:20:17 +0300

    hover helper made multifunctional

--- bonus_task_helpers.rb
+++ bonus_task_helpers.rb
@@ -4,4 +4,24 @@ module BonusTaskHelpers
     @driver.navigate.to(url)
   end
 
+  def hover_on_avatar_of_user(user_number)
+
+    navigate_to "https://the-internet.herokuapp.com/hovers"
+    link_to_profile = find_element_by_css("a[href='/users/#{user_number.to_s}']")
+
+    case user_number
+      when 1
+        user_avatar = find_element_by_css('.figure')
+      when 2
+        user_avatar = find_element_by_css('.figure + .figure')
+      when 3
+        user_avatar = find_element_by_css('.figure + .figure + .figure')
+      else
+        raise 'Not available user specified'
+    end
+
+    @driver.action.move_to(user_avatar).perform
+    link_to_profile
+  end
+
 end
