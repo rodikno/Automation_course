6d55c290217ab1f55fc6d4b470decd5cb526ad11
Rodion Baronov <rodikno@gmail.com>
2016-07-09 17:45:52 +0300

    Made max_num_of_digits_in_a_row much shorter

--- Main_Tasks/Main_Data_Classes/main_data_classes_task.rb
+++ Main_Tasks/Main_Data_Classes/main_data_classes_task.rb
@@ -16,17 +16,8 @@ end
 
 
 def max_num_of_digits_in_a_row(string)
-  a = 0
-  array = Array.new
-  string.each_char do |char|
-    if is_digit(char)
-      a.next
-    else
-      a = 0
-    end
-    array.push a
-  end
-  array.max
+  array = string.scan(/[0-9]*/)
+  array.max_by(&:length)
 end
 
 def max_number_from_string(string)
@@ -35,5 +26,3 @@ def max_number_from_string(string)
   new = array.map { |elem| elem.to_i}
   new.max
 end
-
-print max_number_from_string('5545sd6455600s44d454sd45s')
