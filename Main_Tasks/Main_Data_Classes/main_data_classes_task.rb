0140b6336b58a65252110195122d3ee58b44cf15
Rodion Baronov <rodikno@gmail.com>
2016-07-09 17:32:29 +0300

    Method finding max number from given string

--- Main_Tasks/Main_Data_Classes/main_data_classes_task.rb
+++ Main_Tasks/Main_Data_Classes/main_data_classes_task.rb
@@ -15,12 +15,12 @@ def sum_of_digits(number)
 end
 
 
-def max_num_of_digits_in_a_row(some_string)
+def max_num_of_digits_in_a_row(string)
   a = 0
   array = Array.new
-  some_string.each_char do |char|
+  string.each_char do |char|
     if is_digit(char)
-      a += 1
+      a.next
     else
       a = 0
     end
@@ -29,4 +29,11 @@ def max_num_of_digits_in_a_row(some_string)
   array.max
 end
 
-puts max_num_of_digits_in_a_row('sdsssdfs5656ssd')
+def max_number_from_string(string)
+  array = string.scan(/[0-9]*/)
+  array.delete('')
+  new = array.map { |elem| elem.to_i}
+  new.max
+end
+
+print max_number_from_string('5545sd6455600s44d454sd45s')
