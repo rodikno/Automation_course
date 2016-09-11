ff75a5fb4d462f747ea68ae1758ea4da3b30d441
Rodion Baronov <rodikno@gmail.com>
2016-09-11 23:02:13 +0300

    Added TrelloBoard Class

---
+++ Main_Tasks/Object_Class_Method_Variable/trello_board.rb
@@ -0,0 +1,23 @@
+
+class TrelloBoard
+
+  attr_accessor :name, :background_color
+  attr_reader :lists
+
+  def initialize(board_name)
+    @name = board_name
+    @lists = Array.new
+    @background_color = "Black"
+  end
+
+  def add_list(list)
+    lists << list
+  end
+
+  def add_lists(lists_array)
+    lists_array.each do |list|
+      lists << list
+    end
+  end
+
+end
