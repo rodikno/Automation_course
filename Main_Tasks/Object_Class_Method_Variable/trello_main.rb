1d249dd302b1e9470fb8af6c5b8d6d93ca2572e4
Rodion Baronov <rodikno@gmail.com>
2016-09-11 19:27:25 +0300

    Refactored model to support nice approach for making relations

--- Main_Tasks/Object_Class_Method_Variable/trello_main.rb
+++ Main_Tasks/Object_Class_Method_Variable/trello_main.rb
@@ -4,6 +4,8 @@ first_user = User.new(username)
 #create a board for this user
 main_board = Board.new(first_user)
 
+first_user.add_board(main_board)
+
 
 #add 3 lists to the board
 lists = List.generate(list_name1, list_name2, list_name3)
@@ -11,9 +13,8 @@ main_board.add_lists(lists)
 
 #add a card to specific list on a board
 card = Card.new(title)
-main_board.add_card(card, list_name)
+lists[:todo].add_card(card)
 
 #add a comment for a card
 comment = Comment.new
-main_board.add_comment(comment, list_name, card_name)
-main_board.lists(:todo).cards(:card1).add_comment(comment_text)
+card.add_comment(comment)
