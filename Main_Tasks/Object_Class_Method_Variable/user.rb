4c3968ab0f6d69389236e264218a0fef99739297
Rodion Baronov <rodikno@gmail.com>
2016-09-11 23:05:39 +0300

    Changed order of lines (attr_accessor is on the beginning of class definition now)

--- Main_Tasks/Object_Class_Method_Variable/user.rb
+++ Main_Tasks/Object_Class_Method_Variable/user.rb
@@ -1,16 +1,26 @@
 require 'faker'
+require_relative 'trello_board'
+
 class User
+
+  attr_accessor :username, :first_name, :last_name, :email
 
   def initialize(username = "User" + rand(999))
     @username = username
     @first_name = Faker::Name.first_name
     @last_name = Faker::Name.last_name
     @email = Faker::Internet.email(@first_name)
+    @boards = Array.new
   end
-
-  attr_accessor :username, :first_name, :last_name, :email
 
   def add_board(board)
-    board.add_user(username)
+    @boards << board
+    print @boards.flatten.to_s
   end
+
+  def create_board(board_name)
+    board = TrelloBoard.new(board_name)
+    boards << board  
+  end
+    
 end
