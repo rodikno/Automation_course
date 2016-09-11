8f7ffa47d5460c802e5007f1a7cec9eec6061058
Rodion Baronov <rodikno@gmail.com>
2016-09-11 19:28:35 +0300

    Class 'Board' added with initialize and add_lists method, faker gem added to Gemfile

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'http://rubygems.org'
 gem 'test-unit'
 gem 'selenium-webdriver'
+gem 'faker', '~> 1.6', '>= 1.6.3'
--- Main_Tasks/Object_Class_Method_Variable/board.rb
+++ Main_Tasks/Object_Class_Method_Variable/board.rb
@@ -1,3 +1,17 @@
 
 class Board
+  def initialize(board_name)
+    @name = Faker::Hacker.noun
+    @lists = Array.new
+    @background_color = "Black"
+  end
+
+  attr_accessor :name, :lists, :background_color
+
+  def add_lists(lists_array)
+    lists_array.each do |list|
+      lists << list
+    end
+  end
+
 end
