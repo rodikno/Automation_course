8c0aae9eca7bd1967e8fd724bc327a8d6d4e222a
Rodion Baronov <rodikno@gmail.com>
2016-09-11 19:29:09 +0300

    Class 'User' added with method add_board

--- Main_Tasks/Object_Class_Method_Variable/user.rb
+++ Main_Tasks/Object_Class_Method_Variable/user.rb
@@ -1,3 +1,16 @@
-
+require 'faker'
 class User
+
+  def initialize(username = "User" + rand(999))
+    @username = username
+    @first_name = Faker::Name.first_name
+    @last_name = Faker::Name.last_name
+    @email = Faker::Internet.email(@first_name)
+  end
+
+  attr_accessor :username, :first_name, :last_name, :email
+
+  def add_board(board)
+    board.add_user(username)
+  end
 end
