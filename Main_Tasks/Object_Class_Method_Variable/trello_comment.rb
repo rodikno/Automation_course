2aafaba4a3676199f3717901bded7e9561a08e9e
Rodion Baronov <rodikno@gmail.com>
2016-09-14 01:21:03 +0300

    Prepared TrelloComment for creating from TrelloUser instance method (properties @parent_card, @author added)

--- Main_Tasks/Object_Class_Method_Variable/trello_comment.rb
+++ Main_Tasks/Object_Class_Method_Variable/trello_comment.rb
@@ -3,11 +3,12 @@ require 'date'
 class TrelloComment
 
   attr_accessor :text
-  attr_reader :created_date
+  attr_reader :author, :created_date
 
-  def initialize
-    @text = String.new
-    @author = String.new
+  def initialize(text = "", user, parent_card)
+    @text = text
+    @parent_card = parent_card
+    @author = user
     @created_date = Time.now.strftime("%d/%m/%Y %H:%M")
   end
 
