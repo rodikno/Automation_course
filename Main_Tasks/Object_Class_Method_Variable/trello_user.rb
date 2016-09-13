f623cae99b69faab71af4ee3c6f183aac23202d2
Rodion Baronov <rodikno@gmail.com>
2016-09-14 00:53:05 +0300

    @boards_owned property created which is a hash looks like {:board_id => board} to store multiple boards and access them easily by unique id

--- Main_Tasks/Object_Class_Method_Variable/trello_user.rb
+++ Main_Tasks/Object_Class_Method_Variable/trello_user.rb
@@ -14,26 +14,36 @@ class TrelloUser
     @last_name = Faker::Name.last_name
     @email = Faker::Internet.email(@first_name)
     @biography = Faker::Hipster.sentence(5)
-    @boards = Array.new
+    @boards_owned = Hash.new
+  end
+  
+  def add_board_to_user_boards(board)
+    board_id_symbol = board.board_id.to_s.to_sym
+    @boards_owned[board_id_symbol] = board
   end
 
   def create_board(board_name)
     board = TrelloBoard.new(board_name, self)
-    @boards << board
+    add_board_to_user_boards(board)
     board
   end
 
   def join_board(board)
-    @boards << board
+    add_board_to_user_boards(board)
     board.add_member(self)
   end
 
-  def leave_board(board)
-    @boards.delete(board)
-    board.remove_member(self)
+  def get_all_board_ids
+
   end
 
-  def create_list(list_name, board_where)
+  # Need to rework this to support woking with hash @boards_owned
+  # def leave_board(board_id)
+  #   @boards_owned.delete(board)
+  #   board.remove_member(self)
+  # end
+
+  def create_list(list_name, board_id_where)
     board_index = @boards.index(board_where)
     if board_index
       required_board = @boards.fetch(board_index)
