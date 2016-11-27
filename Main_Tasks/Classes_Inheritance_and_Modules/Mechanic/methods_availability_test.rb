e113d5b009e0e1016c3e92a740d40070357bf71d
Rodion Baronov <rodikno@gmail.com>
2016-11-27 20:02:39 +0200

    Edited mechanic unit tests

--- Main_Tasks/Classes_Inheritance_and_Modules/Mechanic/methods_availability_test.rb
+++ Main_Tasks/Classes_Inheritance_and_Modules/Mechanic/methods_availability_test.rb
@@ -8,18 +8,18 @@ class MethodsAvailabilityTest < Test::Unit::TestCase
     @simon = Mechanic.new(27, false)
   end
 
-  def test_public_method
+  def test_public_method_positive
     assert(@john.respond_to? :say_swear)
   end
 
-  def test_private_method
+  def test_private_method_negative
     assert_false(@john.respond_to? :get_random_swear)
   end
 
-  def test_protected_method
+  def test_protected_method_positive
     #So let's ask Simon about how much Vodka he can drink at once
     #Please remember: you can get this info only if you're a mechanic too
-    what_john_knows_about_simon = @john.send(:how_much_drinks, @simon)
+    what_john_knows_about_simon = @john.how_much_drinks(@simon)
     # I've created private method to get the same property directly just to get something to assert
     what_simon_knows_about_himself = @simon.send(:alcohol_max_consumption)
 
