2707d8cd5750f26ccdedc93f74b63d7b0556c59a
Rodion Baronov <rodikno@gmail.com>
2016-11-28 20:27:20 +0200

    Added methods visibility unit tests

--- Main_Tasks/Classes_Inheritance_and_Modules/Mechanic/methods_availability_test.rb
+++ Main_Tasks/Classes_Inheritance_and_Modules/Mechanic/methods_availability_test.rb
@@ -8,15 +8,23 @@ class MethodsAvailabilityTest < Test::Unit::TestCase
     @simon = Mechanic.new(27, false)
   end
 
-  def test_public_method_positive
-    assert(@john.respond_to? :say_swear)
+  def test_public_positive
+    assert_nothing_raised do
+      @john.say_swear
+    end
   end
 
-  def test_private_method_negative
-    assert_false(@john.respond_to? :get_random_swear)
+  def test_private_positive
+    assert(@john.send(:get_random_swear))
   end
 
-  def test_protected_method_positive
+  def test_private_negative
+    assert_raises NoMethodError do
+      @john.get_random_swear
+    end
+  end
+
+  def test_protected_positive
     #So let's ask Simon about how much Vodka he can drink at once
     #Please remember: you can get this info only if you're a mechanic too
     what_john_knows_about_simon = @john.how_much_drinks(@simon)
@@ -25,4 +33,11 @@ class MethodsAvailabilityTest < Test::Unit::TestCase
 
     assert_equal(what_simon_knows_about_himself, what_john_knows_about_simon)
   end
+
+  def test_protected_negative
+    assert_raises NoMethodError do
+      @john.get_alcohol_limit
+    end
+  end
+
 end
